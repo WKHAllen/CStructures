36256794ede61f2e290b2e81fcfd09d30441fcd1
WKHAllen <wkhallen@gmail.com>
2020-10-09 12:21:51 -0500

    Add C++ support

--- src/cstructures.h
+++ src/cstructures.h
@@ -7,11 +7,20 @@
 #ifndef _CSTRUCTURES_H_
 #define _CSTRUCTURES_H_
 
+#ifdef _cplusplus
+extern "C" {
+#endif
+
 #include "stack.h"
 #include "queue.h"
 #include "linkedlist.h"
 #include "list.h"
 #include "hashtable.h"
 #include "binarytree.h"
+#include "string.h"
+
+#ifdef _cplusplus
+}
+#endif
 
 #endif /* _CSTRUCTURES_H_ */
